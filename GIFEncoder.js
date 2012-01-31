c75790500d5b993e79146f941b36715794330261
Forrest Oliphant <github.com@sembiki.com>
2012-01-31 16:42:37 +0200

    working on transparency, not quite working yet

--- GIFEncoder.js
+++ GIFEncoder.js
@@ -18,8 +18,9 @@
 	
 	GIFEncoder = function()
 	{
-	    for(var i = 0, chr = {}; i < 256; i++)
+	    for(var i = 0, chr = {}; i < 256; i++) {
         chr[i] = String.fromCharCode(i);
+	    }
         
       function ByteArray(){
         this.bin = [];
@@ -27,20 +28,23 @@
 
       ByteArray.prototype.getData = function(){
         
-	      for(var v = '', l = this.bin.length, i = 0; i < l; i++)
+	      for(var v = '', l = this.bin.length, i = 0; i < l; i++) {
           v += chr[this.bin[i]];
+	      }
         return v;
       }
       ByteArray.prototype.writeByte = function(val){
         this.bin.push(val);
       }
       ByteArray.prototype.writeUTFBytes = function(string){
-        for(var l = string.length, i = 0; i < l; i++)
+        for(var l = string.length, i = 0; i < l; i++) {
           this.writeByte(string.charCodeAt(i));
+        }
       }
       ByteArray.prototype.writeBytes = function(array, offset, length){
-        for(var l = length || array.length, i = offset||0; i < l; i++)
+        for(var l = length || array.length, i = offset||0; i < l; i++) {
           this.writeByte(array[i]);
+        }
       }
 	
 	    var exports = {};
@@ -91,7 +95,7 @@
 		var setDispose = exports.setDispose = function setDispose(code/*int*/)/*void*/ 
 		{
 			
-			if (code >= 0) dispose = code;
+			if (code >= 0) { dispose = code; }
 			
 		}
 		
@@ -108,7 +112,7 @@
 		var setRepeat = exports.setRepeat = function setRepeat(iter/*int*/)/*void*/ 
 		{
 			
-			if (iter >= 0) repeat = iter;
+			if (iter >= 0) { repeat = iter; }
 			
 		}
 		
@@ -147,29 +151,31 @@
 		    var ok/*Boolean*/ = true;
 			
 		    try {
-				if(!is_imageData){
-				  image = im.getImageData(0,0, im.canvas.width, im.canvas.height).data;
-				  if (!sizeSet) setSize(im.canvas.width, im.canvas.height);
-				}else{
-				  image = im;
-				}
-				getImagePixels(); // convert to correct format if necessary
-				analyzePixels(); // build color table & map pixels
-				
-				if (firstFrame) 
-				{
-					writeLSD(); // logical screen descriptior
-					writePalette(); // global color table
-					if (repeat >= 0) 
-					{
-						// use NS app extension to indicate reps
-						writeNetscapeExt();
+					if(!is_imageData){
+					  image = im.getImageData(0,0, im.canvas.width, im.canvas.height).data;
+					  if (!sizeSet) {
+						  setSize(im.canvas.width, im.canvas.height);
+						}
+					}else{
+					  image = im;
 					}
+					getImagePixels(); // convert to correct format if necessary
+					analyzePixels(); // build color table & map pixels
+					
+					if (firstFrame) 
+					{
+						writeLSD(); // logical screen descriptior
+						writePalette(); // global color table
+						if (repeat >= 0) 
+						{
+							// use NS app extension to indicate reps
+							writeNetscapeExt();
+						}
 		      }
 			  
-			  writeGraphicCtrlExt(); // write graphic control extension
+				  writeGraphicCtrlExt(); // write graphic control extension
 		      writeImageDesc(); // image descriptor
-		      if (!firstFrame) writePalette(); // local color table
+		      if (!firstFrame) { writePalette(); } // local color table
 		      writePixels(); // encode and write pixel data
 		      firstFrame = false;
 		    } catch (e/*Error*/) {
@@ -187,7 +193,7 @@
 		
 		var finish = exports.finish = function finish()/*Boolean*/
 		{
-		    if (!started) return false;
+		    if (!started) { return false; }
 		    var ok/*Boolean*/ = true;
 		    started = false;
 		    try {
@@ -207,16 +213,14 @@
 		
 		var reset = function reset ( )/*void*/
 		{
-			
 			// reset for subsequent use
 			transIndex = 0;
 			image = null;
-		    pixels = null;
-		    indexedPixels = null;
-		    colorTab = null;
-		    closeStream = false;
-		    firstFrame = true;
-			
+			pixels = null;
+			indexedPixels = null;
+			colorTab = null;
+			closeStream = false;
+			firstFrame = true;
 		}
 
 		/**
@@ -267,9 +271,9 @@
 			if (started && !firstFrame) return;
 		    width = w;
 		    height = h;
-		    if (width < 1)width = 320;
-		    if (height < 1)height = 240;
-		    sizeSet = true
+		    if (width < 1) { width = 320; }
+		    if (height < 1) { height = 240; }
+		    sizeSet = true;
 			
 		}
 		
@@ -284,17 +288,17 @@
 		var start = exports.start = function start()/*Boolean*/
 		{
 			
-			reset(); 
-		    var ok/*Boolean*/ = true;
-		    closeStream = false;
-		    out = new ByteArray;
-		    try {
-		      out.writeUTFBytes("GIF89a"); // header
-		    } catch (e/*Error*/) {
-		      ok = false;
-		    }
-			
-		    return started = ok;
+			reset();
+			var ok/*Boolean*/ = true;
+			closeStream = false;
+			out = new ByteArray;
+			try {
+			  out.writeUTFBytes("GIF89a"); // header
+			} catch (e/*Error*/) {
+			  ok = false;
+			}
+
+			return started = ok;
 			
 		}
 		
@@ -306,25 +310,25 @@
 		{
 		    
 			var len/*int*/ = pixels.length;
-		    var nPix/*int*/ = len / 3;
-		    indexedPixels = [];
-		    var nq/*NeuQuant*/ = new NeuQuant(pixels, len, sample);
-		    // initialize quantizer
-		    colorTab = nq.process(); // create reduced palette
-		    // map image pixels to new palette
-		    var k/*int*/ = 0;
-		    for (var j/*int*/ = 0; j < nPix; j++) {
-		      var index/*int*/ = nq.map(pixels[k++] & 0xff, pixels[k++] & 0xff, pixels[k++] & 0xff);
-		      usedEntry[index] = true;
-		      indexedPixels[j] = index;
-		    }
-		    pixels = null;
-		    colorDepth = 8;
-		    palSize = 7;
-		    // get closest match to transparent color if specified
-		    if (transparent != null) {
-		      transIndex = findClosest(transparent);
-		    }
+			var nPix/*int*/ = len / 3;
+			indexedPixels = [];
+			var nq/*NeuQuant*/ = new NeuQuant(pixels, len, sample);
+			// initialize quantizer
+			colorTab = nq.process(); // create reduced palette
+			// map image pixels to new palette
+			var k/*int*/ = 0;
+			for (var j/*int*/ = 0; j < nPix; j++) {
+				var index/*int*/ = nq.map(pixels[k++] & 0xff, pixels[k++] & 0xff, pixels[k++] & 0xff);
+				usedEntry[index] = true;
+				indexedPixels[j] = index;
+			}
+			pixels = null;
+			colorDepth = 8;
+			palSize = 7;
+			// get closest match to transparent color if specified
+			if (transparent != null) {
+				transIndex = findClosest(transparent);
+			}
 		}
 		
 		/**
@@ -335,58 +339,61 @@
 		var findClosest = function findClosest(c/*Number*/)/*int*/
 		{
 			
-			if (colorTab == null) return -1;
-		    var r/*int*/ = (c & 0xFF0000) >> 16;
-		    var g/*int*/ = (c & 0x00FF00) >> 8;
-		    var b/*int*/ = (c & 0x0000FF);
-		    var minpos/*int*/ = 0;
-		    var dmin/*int*/ = 256 * 256 * 256;
-		    var len/*int*/ = colorTab.length;
-			
-		    for (var i/*int*/ = 0; i < len;) {
-		      var dr/*int*/ = r - (colorTab[i++] & 0xff);
-		      var dg/*int*/ = g - (colorTab[i++] & 0xff);
-		      var db/*int*/ = b - (colorTab[i] & 0xff);
-		      var d/*int*/ = dr * dr + dg * dg + db * db;
-		      var index/*int*/ = i / 3;
-		      if (usedEntry[index] && (d < dmin)) {
-		        dmin = d;
-		        minpos = index;
-		      }
-		      i++;
-		    }
-		    return minpos;
+			if (colorTab == null) {
+				return -1;
+			}
+
+	    var r/*int*/ = (c & 0xFF0000) >> 16;
+	    var g/*int*/ = (c & 0x00FF00) >> 8;
+	    var b/*int*/ = (c & 0x0000FF);
+	    var minpos/*int*/ = 0;
+	    var dmin/*int*/ = 256 * 256 * 256;
+	    var len/*int*/ = colorTab.length;
+		
+	    for (var i/*int*/ = 0; i < len;) {
+				// var tr = colorTab[i] & 0xff;
+				// var tg = colorTab[i+1] & 0xff;
+				// var tb = colorTab[i+2] & 0xff;
+				// if (r===tr && g===tg && b===tb) {
+				// 	i+=3;
+				// 	var index = (i / 3) - 1;
+				// 	if (usedEntry[index]) {
+				// 	console.log("skip:", index);
+				// 		return index;
+				// 	}
+				// }
+	      var dr/*int*/ = r - (colorTab[i++] & 0xff);
+	      var dg/*int*/ = g - (colorTab[i++] & 0xff);
+	      var db/*int*/ = b - (colorTab[i++] & 0xff);
+	      var d/*int*/ = dr * dr + dg * dg + db * db;
+	      var index/*int*/ = (i / 3) - 1;
+	      if (usedEntry[index] && (d < dmin)) {
+	        dmin = d;
+	        minpos = index;
+	      }
+	    }
+	    return minpos;
 			
 		}
 		
 		/**
-		* Extracts image pixels into byte array "pixels
+		* Extracts image pixels into byte array "pixels"
 		*/
 		
 		var getImagePixels = function getImagePixels()/*void*/
 		{
-		    
-		    var w/*int*/ = width;
-		    var h/*int*/ = height;
-		    pixels = [];
-  			var data = image;
-		    var count/*int*/ = 0;
-		    
-		    for ( var i/*int*/ = 0; i < h; i++ )
-		    {
-		    	
-		    	for (var j/*int*/ = 0; j < w; j++ )
-		    	{
-		    		
-	        		var b = (i*w*4)+j*4;
-	        		pixels[count++] = data[b];
-	        		pixels[count++] = data[b+1];
-	        		pixels[count++] = data[b+2];
-		    		
-		    	}
-		    	
-		    }
-		    
+			var w/*int*/ = width;
+			var h/*int*/ = height;
+			pixels = [];
+			var data = image;
+			var count/*int*/ = 0;
+
+			for (var i=0; i<data.length; i+=4) {
+				pixels[count++] = data[i];
+				pixels[count++] = data[i+1];
+				pixels[count++] = data[i+2];
+				// Skip alpha
+			}
 		}
 		
 		/**
@@ -430,24 +437,24 @@
 		var writeImageDesc = function writeImageDesc()/*void*/
 		{
 		  	
-		    out.writeByte(0x2c); // image separator
-		   	WriteShort(0); // image position x,y = 0,0
-		    WriteShort(0);
-		    WriteShort(width); // image size
-		    WriteShort(height);
+			out.writeByte(0x2c); // image separator
+			WriteShort(0); // image position x,y = 0,0
+			WriteShort(0);
+			WriteShort(width); // image size
+			WriteShort(height);
 
-		    // packed fields
-		    if (firstFrame) {
-		      // no LCT - GCT is used for first (or only) frame
-		      out.writeByte(0);
-		    } else {
-		      // specify normal LCT
-		      out.writeByte(0x80 | // 1 local color table 1=yes
-		          0 | // 2 interlace - 0=no
-		          0 | // 3 sorted - 0=no
-		          0 | // 4-5 reserved
-		          palSize); // 6-8 size of color table
-		    }
+			// packed fields
+			if (firstFrame) {
+			  // no LCT - GCT is used for first (or only) frame
+			  out.writeByte(0);
+			} else {
+			  // specify normal LCT
+			  out.writeByte(0x80 | // 1 local color table 1=yes
+					0 | // 2 interlace - 0=no
+					0 | // 3 sorted - 0=no
+					0 | // 4-5 reserved
+					palSize); // 6-8 size of color table
+			}
 		}
 		
 		/**
@@ -458,16 +465,16 @@
 		{
 			
 			// logical screen size
-		    WriteShort(width);
-		    WriteShort(height);
-		    // packed fields
-		    out.writeByte((0x80 | // 1 : global color table flag = 1 (gct used)
-		        0x70 | // 2-4 : color resolution = 7
-		        0x00 | // 5 : gct sort flag = 0
-		        palSize)); // 6-8 : gct size
-		
-		    out.writeByte(0); // background color index
-		    out.writeByte(0); // pixel aspect ratio - assume 1:1
+			WriteShort(width);
+			WriteShort(height);
+			// packed fields
+			out.writeByte((0x80 | // 1 : global color table flag = 1 (gct used)
+				0x70 | // 2-4 : color resolution = 7
+				0x00 | // 5 : gct sort flag = 0
+				palSize)); // 6-8 : gct size
+
+			out.writeByte(0); // background color index
+			out.writeByte(0); // pixel aspect ratio - assume 1:1
 			
 		}
 		
@@ -477,16 +484,14 @@
 		
 		var writeNetscapeExt = function writeNetscapeExt()/*void*/
 		{
-			
-		    out.writeByte(0x21); // extension introducer
-		    out.writeByte(0xff); // app extension label
-		    out.writeByte(11); // block size
-		    out.writeUTFBytes("NETSCAPE" + "2.0"); // app id + auth code
-		    out.writeByte(3); // sub-block size
-		    out.writeByte(1); // loop sub-block id
-		    WriteShort(repeat); // loop count (extra iterations, 0=repeat forever)
-		    out.writeByte(0); // block terminator
-		
+			out.writeByte(0x21); // extension introducer
+			out.writeByte(0xff); // app extension label
+			out.writeByte(11); // block size
+			out.writeUTFBytes("NETSCAPE" + "2.0"); // app id + auth code
+			out.writeByte(3); // sub-block size
+			out.writeByte(1); // loop sub-block id
+			WriteShort(repeat); // loop count (extra iterations, 0=repeat forever)
+			out.writeByte(0); // block terminator
 		}
 		
 		/**
@@ -495,18 +500,15 @@
 		
 		var writePalette = function writePalette()/*void*/
 		{
-		    out.writeBytes(colorTab);
-		    var n/*int*/ = (3 * 256) - colorTab.length;
-		    for (var i/*int*/ = 0; i < n; i++) out.writeByte(0);
-			
+			out.writeBytes(colorTab);
+			var n/*int*/ = (3 * 256) - colorTab.length;
+			for (var i/*int*/ = 0; i < n; i++) { out.writeByte(0); }
 		}
 		
 		var WriteShort = function WriteShort (pValue/*int*/)/*void*/
-		{  	
-			
-		  	out.writeByte( pValue & 0xFF );
-		  	out.writeByte( (pValue >> 8) & 0xFF);
-			
+		{
+			out.writeByte( pValue & 0xFF );
+			out.writeByte( (pValue >> 8) & 0xFF);
 		}
 		
 		/**
@@ -515,10 +517,8 @@
 		
 		var writePixels = function writePixels()/*void*/
 		{
-			
-		    var myencoder/*LZWEncoder*/ = new LZWEncoder(width, height, indexedPixels, colorDepth);
-		    myencoder.encode(out);
-			
+			var myencoder/*LZWEncoder*/ = new LZWEncoder(width, height, indexedPixels, colorDepth);
+			myencoder.encode(out);
 		}
 		
 		/**
@@ -526,9 +526,7 @@
 		*/
 		var stream = exports.stream = function stream ( )/*ByteArray*/
 		{
-			
-			return out; 
-			
+			return out;
 		}
 		
 		var setProperties = exports.setProperties = function setProperties(has_start, is_first){
